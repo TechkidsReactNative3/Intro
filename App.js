96d00fa92abd46f35b801ed347264cd7093c9e69
Qk Lahpita <techkids.a12@gmail.com>
2018-07-04 17:07:51 +0700

    Done props

--- App.js
+++ App.js
@@ -1,32 +1,20 @@
 import React, { Component } from 'react'
 import { Text, View, StyleSheet } from 'react-native'
 
+import ColorButton from './ColorButton';
+
 class App extends Component {
-  state = {}
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.text1}>Hello</Text>
-        <Text style={styles.text2}>Hello</Text>
+      <View >
+        <ColorButton color='yellow' />
+        <ColorButton color='red' />
+        <ColorButton color='green' />
+        <ColorButton color='blue' />
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  text1: {
-    flex: 2,
-    backgroundColor: 'yellow'
-  },
-  text2: {
-    flex: 1,
-    backgroundColor: 'green'
-  },
-  container: {
-    backgroundColor: 'red',
-    flexDirection: 'row',
-    flex: 1
-  }
-});
 
 export default App;
---
+++ ColorButton.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import { Text, View, TouchableOpacity } from 'react-native'
+
+class ColorButton extends Component {
+  state = {}
+  render() {
+    return (
+      <TouchableOpacity style={{
+        width: 150,
+        height: 150,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: this.props.color
+      }}
+      >
+        <Text>{this.props.color}</Text>
+      </TouchableOpacity>
+    );
+  }
+}
+
+export default ColorButton;
